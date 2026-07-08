bac1cc6970bfec94055d0e2753de68131dc88aeb
Nahuel Perea <nahuel.perea.dev@gmail.com>
2026-07-08 00:16:38 -0300

    git commit de interface.py

--- interface.py
+++ interface.py
@@ -4,4 +4,7 @@ from consuta import respuesta
 def chat(message, history):
     return respuesta(message)
 
-gr.ChatInterface(fn=chat).launch()
+gr.ChatInterface(fn=chat).launch(
+    server_name="0.0.0.0",
+    server_port=7860
+)
